01ee6477a206d3d65006cedf0eef03449a2d3f73
Tedros Alem <tedsaasfaha@gmail.com>
2024-10-17 12:20:04 +0300

    [Code Refactoring] refactoring MortgageCalculator &  MortgageReport classes

--- Mortgage Calculator/src/MortgageCalculator.java
+++ Mortgage Calculator/src/MortgageCalculator.java
@@ -3,8 +3,13 @@ public class MortgageCalculator {
     private float annualInterest;
     private byte years;
 
-    public double calculateMortgage() {
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
 
+    public double calculateMortgage() {
         float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
         float numberOfPayments = years * Main.MONTHS_IN_YEAR;
 
@@ -24,5 +29,9 @@ public class MortgageCalculator {
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         return balance;
+    }
+
+    public byte getYears() {
+        return years;
     }
 }
