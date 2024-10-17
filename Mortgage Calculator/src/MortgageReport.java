00022b6b1602f51fe9f5314dd3873ae6a2c87a3f
Tedros Alem <tedsaasfaha@gmail.com>
2024-10-17 11:54:18 +0300

    [refactoring MortgageCalculator class] created MortgageCalculator class

--- Mortgage Calculator/src/MortgageReport.java
+++ Mortgage Calculator/src/MortgageReport.java
@@ -3,7 +3,7 @@ import java.text.NumberFormat;
 public class MortgageReport {
 
     public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = Main.calculateMortgage(principal, annualInterest, years);
+        double mortgage = new MortgageCalculator().calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
@@ -16,7 +16,7 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = Main.calculateBalance(principal, annualInterest, years, month);
+            double balance = new MortgageCalculator().calculateBalance(month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
